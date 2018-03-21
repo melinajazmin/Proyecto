79ba516b528cf4e4be82065f95256f5ef8471bba
rsbertoa90 <rsbertoa90@gmail.com>
2018-03-21 12:08:19 -0300

    + responsive en quienes somos. + agregado el recordame y recuperar contraseña en modal login

--- partials/modales.php
+++ partials/modales.php
@@ -57,8 +57,11 @@
             <input class="form-control form-control-lg" type="email" placeholder="TUMAIL@EMAIL.COM">
             <input class="form-control form-control-lg" type="password" placeholder="PASSWORD">
           </div>
-            <div class="form-group d-flex justify-content-center">
-              <button type="submit"class="btn btn-primary btn-lg mb-3" name="submt" data-toggle="modal" data-target="#login-modal">INGRESA</button>
+            <div class="form-group d-flex flex-wrap align-content-center justify-content-center">
+              <button type="submit"class="col-12 btn btn-primary btn-lg mb-3" name="submt" data-toggle="modal" data-target="#login-modal">INGRESA</button>
+              <input type="checkbox" class="" name="recordar" value=""><span class="text-black">Recordarme |
+                <a class="text-primary" href="#">Olvidaste tu contraseña?</a>
+              </span>
             </div>
 
             <div class="modal-footer">
--- quienes.php
+++ quienes.php
@@ -16,21 +16,21 @@
             <h1 class="display-3 col-12"><?=$navigation[$current]?></h1>
             <p class="lead col-12">En <b class="font-weight-bold">Eventr</b> creemos que para brindar un servicio de calidad, nuestro equipo debe ser parte de ese mundo. Es por eso que todos en Eventr somos músico o estamos relacionados con la música. Amamos la música. Es parte de nuestra vida.</p>
 
-            <div class="card" style="width: 33%;">
+            <div class="card col-12 col-lg-4">
                 <img class="card-img-top" src="images/meli.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Meli | <span class="grisado">UX</span></h5>
                     <p class="card-text">CrossFitter y UX designer. El diseño y la usabilidad son pilares fundamentales en mi vida</p>
                 </div>
             </div>
-            <div class="card" style="width: 33%;">
+              <div class="card col-12 col-lg-4">
                 <img class="card-img-top" src="images/rodri.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Rodri | <span class="grisado">JS Master</span></h5>
                     <p class="card-text">Saxofonista y fanático del ReactJS. El backend es mi vida</p>
                 </div>
             </div>
-            <div class="card" style="width: 33%;">
+            <div class="card col-12 col-lg-4">
                 <img class="card-img-top" src="images/pato.jpg" alt="Card image cap">
                 <div class="card-body">
                     <h5 class="card-title">Pato | <span class="grisado">The Other Guy</span></h5>
@@ -40,7 +40,7 @@
         </div>
 
 
- 
+
 
   </div>
   <!-- /CONTAINER -->
